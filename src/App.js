a6cbca670ce578352f61c598fcdfedbe2135feb1
Anna Levitskaya <anna.levitskaya@polcode.net>
2023-01-17 22:11:21 +0100

    improve handleCreateBook

--- src/App.js
+++ src/App.js
@@ -6,9 +6,15 @@ function App() {
 
   const handleCreateBook = (title) => {
     // BAD CODE
-    books.push({id: 123, title: title});
-    console.log(books);
-    setBooks(books);
+    // books.push({id: 123, title: title});  // modifies an existing array directly, will not be re-rendered
+    // setBooks(books);
+
+    const updatedBooks = [
+      ...books,
+      { id: Math.round(Math.random() * 9999), title },
+    ]; // creates a new array, adds there an existing books and a new one to the end of the array
+
+    setBooks(updatedBooks);
   };
 
   return (
